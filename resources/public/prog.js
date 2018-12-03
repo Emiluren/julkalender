663e3d66c4b66c708d6aa63f30b629278c38057f
Emil Segerbäck <emilsegerback@gmail.com>
2018-12-03 17:48:03 +0100

    Start of simpler js verion

---
+++ resources/public/prog.js
@@ -0,0 +1,10 @@
+let treeSvg = "\n \
+<rect x=\"0\" y=\"0\" width=\"100\" height=\"100\" fill=\"black\"></rect>\n \
+<line x1=\"10\" x2=\"10\" y1=\"10\" y2=\"20\" stroke=\"yellow\"></line>\n \
+<line x1=\"5\" x2=\"15\" y1=\"15\" y2=\"15\" stroke=\"yellow\"></line>\n \
+<line x1=\"80\" x2=\"80\" y1=\"17\" y2=\"27\" stroke=\"yellow\"></line>\n \
+<line x1=\"75\" x2=\"85\" y1=\"22\" y2=\"22\" stroke=\"yellow\"></line>\n \
+<path d=\"M0 90 C20 85, 40 87, 100 82 V100 H0\" fill=\"white\"></path>\n\n \
+<polygon points=\"20 80, 40 60, 25 60, 50 30, 75 60, 60 60, 80 80\" fill=\"green\"></polygon>\n \
+<rect x=\"45\" y=\"80\" width=\"10\" height=\"15\" fill=\"brown\"></rect>\n \
+"
